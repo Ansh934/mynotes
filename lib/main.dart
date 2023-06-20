51f7c24e1668f7d48cd15ed8eb42a1f1158855dc
Ansh Soni <soni.ansh.king09@gmail.com>
2023-06-21 02:46:17 +0530

    [Refactored] NotesView & NewNotesView

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/utilities/loading_screen.dart';
 import 'package:mynotes/views/login_view.dart';
-import 'package:mynotes/views/notes_view.dart';
+import 'package:mynotes/views/notes/notes_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verify_email_view.dart';
 
--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -33,7 +33,7 @@ class _NotesViewState extends State<NotesView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Main UI"),
+        title: const Text("Your Notes"),
         actions: [
           PopupMenuButton<MenuAction>(
             onSelected: (value) async {

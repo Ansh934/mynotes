c09022f65e22a48592f67d0365735f3c08391850
Ansh Soni <soni.ansh.king09@gmail.com>
2023-06-21 03:14:55 +0530

    [Updated] Routes & NotesView & NewNotesView

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,8 @@ import 'dart:developer' as devtools show log;
 import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
-import 'package:mynotes/utilities/loading_screen.dart';
 import 'package:mynotes/views/login_view.dart';
+import 'package:mynotes/views/notes/new_note_view.dart';
 import 'package:mynotes/views/notes/notes_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verify_email_view.dart';
@@ -22,12 +22,11 @@ void main() {
       ),
       home: const HomePage(),
       routes: {
-        homePageRoute: (context) => const HomePage(),
         loginRoute: (context) => const LoginView(),
         registerRoute: (context) => const RegisterView(),
         verifyEmailRoute: (context) => const VerifyEmailView(),
         notesRoute: (context) => const NotesView(),
-        circularLoadingScreenRoute: (context) => const CircleLoadingScreen(),
+        newNoteRoute: (context) => const NewNoteView(),
       },
     ),
   );
--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -35,6 +35,12 @@ class _NotesViewState extends State<NotesView> {
       appBar: AppBar(
         title: const Text("Your Notes"),
         actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed(newNoteRoute);
+            },
+            icon: const Icon(Icons.add),
+          ),
           PopupMenuButton<MenuAction>(
             onSelected: (value) async {
               switch (value) {
